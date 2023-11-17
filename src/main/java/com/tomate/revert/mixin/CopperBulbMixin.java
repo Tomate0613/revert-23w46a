5e16fee51c1c1ae57857c71f6d8905a53d5cd6f9
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2023-11-17 19:34:46 +0100

    Initial commit

---
+++ src/main/java/com/tomate/revert/mixin/CopperBulbMixin.java
@@ -0,0 +1,39 @@
+package com.tomate.revert.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CopperBulbBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(CopperBulbBlock.class)
+public abstract class CopperBulbMixin extends Block {
+    @Shadow
+    @Final
+    public static BooleanProperty POWERED;
+
+    @Shadow
+    public abstract void checkAndFlip(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos);
+
+    public CopperBulbMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Redirect(method = "neighborChanged", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/CopperBulbBlock;checkAndFlip(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;)V"))
+    void tick(CopperBulbBlock instance, BlockState blockState, ServerLevel level, BlockPos blockPos) {
+        level.scheduleTick(blockPos, instance, 1);
+    }
+
+
+    @Override
+    public void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
+        checkAndFlip(blockState, serverLevel, blockPos);
+    }
+}
