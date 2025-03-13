b5d7b545f73988c8029b49a32b116a3e9733efd9
Tomate 06 13 <69756782+Tomate0613@users.noreply.github.com>
2025-03-13 22:36:46 +0100

    Remove unused code

--- src/main/java/com/tomate/revert/mixin/CopperBulbMixin.java
+++ src/main/java/com/tomate/revert/mixin/CopperBulbMixin.java
@@ -6,8 +6,6 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CopperBulbBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,10 +13,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CopperBulbBlock.class)
 public abstract class CopperBulbMixin extends Block {
-    @Shadow
-    @Final
-    public static BooleanProperty POWERED;
-
     @Shadow
     public abstract void checkAndFlip(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos);
 
